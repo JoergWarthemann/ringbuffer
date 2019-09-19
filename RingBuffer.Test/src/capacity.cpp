716885cd159edeaab6521b910044fb50bb7bbb27
JoergWarthemann <joergwarthemann@hotmail.de>
2019-09-19 14:54:12 +0200

    * add capacity tests

--- RingBuffer.Test/src/capacity.cpp
+++ RingBuffer.Test/src/capacity.cpp
@@ -3,7 +3,7 @@
 #include "RingBuffer.h"
 #include <string>
 
-TEST_CASE("Capacity of the ring buffer", "[capacity]")
+TEST_CASE("Capacity of the ring buffer and element count", "[capacity and count]")
 {
 	static const std::size_t sourceSize = 20;
     static const std::size_t destinationSize = 19;
@@ -23,11 +23,11 @@ TEST_CASE("Capacity of the ring buffer", "[capacity]")
 
     REQUIRE(source.size() == sourceSize);
 
-	WHEN("The elements are getting inserted into the ring buffer")
+	WHEN("More elements than the ring buffers capacity are being inserted")
     {
 		destination.insert(source, sourceSize);
 
-		THEN("the size of the ring buffer is correct.")
+		THEN("  the size of the ring buffer is correct.")
         {
             REQUIRE(destination.currentSize() == destinationSize);
         }
@@ -38,9 +38,57 @@ TEST_CASE("Capacity of the ring buffer", "[capacity]")
 		destination.insert(source, sourceSize);
 		destination.reset();
 		
-		THEN("the size of the ring buffer is 0.")
+		THEN("  the size of the ring buffer is 0.")
         {
             REQUIRE(destination.currentSize() == 0);
         }
 	}
+
+    WHEN("A block of elements is copied into the ring buffer destination and then the ring buffer is resized")
+    {
+        // First we insert too many elements into the ring buffer.
+        destination.insert(source, sourceSize);
+
+        // Then we resize the ring buffer
+        destination.reset(sourceSize * 2);
+
+        THEN("  the ring buffer capacity has the right size.")
+        {
+            REQUIRE(sourceSize * 2 == destination.capacity());
+        }
+
+        AND_THEN("  the size of the ring buffer is correct.")
+        {
+            REQUIRE(destination.currentSize() == 0);
+        }
+
+        // Then we twice insert the source.
+        destination.insert(source, sourceSize);
+        destination.insert(source, sourceSize);
+
+        for (std::size_t i = 0; i < sourceSize; ++i)
+        {
+            auto copiedElement = destination.copy(i);
+
+            DYNAMIC_SECTION("  The " << i << ". destination element contains the expected data.")
+            {
+                REQUIRE(sourceControl[sourceSize - i % sourceSize - 1] == copiedElement);
+            }
+        }
+    }
+
+    WHEN("A block of elements is copied into the ring buffer destination")
+    {
+        destination.insert(source, sourceSize);
+
+        THEN("  the ring buffer size does not exceed the source size.")
+        {
+            REQUIRE_FALSE(sourceSize == destination.currentSize());
+        }
+
+        AND_THEN("  the ring buffer capacity and its current size are equal.")
+        {
+            REQUIRE(sourceSize - destination.capacity() == sourceSize - destination.currentSize());
+        }
+    }
 }
